292204367f07f1ec03162290a9d3438a98613ded
AbaoRicky <58324475+Ricky7737@users.noreply.github.com>
2023-08-17 15:26:21 +0800

    2023-8-17 完成.bat與 .vbs
    .bat運行會有cmd跳出視窗
    .vbs可以把.bat在後台執行

---
+++ MainCode/autorun/TwMarket.bat
@@ -0,0 +1,2 @@
+cd e:\DataLearn\StockInformationWriteInGoogleSheet\TWStock
+C:/Users/baoqi/anaconda3/python.exe E:\DataLearn\StockInformationWriteInGoogleSheet\TWStock\TwMarket.py
---
+++ MainCode/autorun/TwMarketBackRun.vbs
@@ -0,0 +1,2 @@
+set ws=WScript.CreateObject("WScript.Shell")
+ws.Run "E:\DataLearn\StockInformationWriteInGoogleSheet\TWStock\autorun\TwMarket.bat",0
